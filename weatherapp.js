2a8b54c51a81ee2f5ffc5b97095a4496bc4f54d7
Leti <lgsainz15@gmail.com>
2017-03-24 12:38:15 -0400

    Update weatherapp.js

--- weatherapp.js
+++ weatherapp.js
@@ -9,37 +9,37 @@ $(document).ready(function() {
 
   // Get geolocation
   if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
-    getWeather();
-    console.log("geolocation worked");
-  }, function(error) {
-      //alternative for chrome - as no longer supports geolocation over http
-      $.getJSON('http://ipinfo.io', function(data) {
-        lat = data.loc.split(',')[0];
-        long = data.loc.split(',')[1];
+     navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
         getWeather();
-      });
-  });
-}
+        console.log("geolocation worked");
+     }, function(error) {
+        //alternative for chrome - as no longer supports geolocation over http
+        $.getJSON('http://ipinfo.io', function(data) {
+           lat = data.loc.split(',')[0];
+           long = data.loc.split(',')[1];
+           getWeather();
+        });
+     });
+  }
 });  
 
 // Get weather information from OpenWeatherMap and populate corresponding elements
 function getWeather() {
   
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=metric" + "&appid=" + key, function(json)   {
-    $("#location").text(json.name + ", " + json.sys.country);
+   $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=metric" + "&appid=" + key, function(json) {
+      
+      $("#location").text(json.name + ", " + json.sys.country);
     
-    // Get temp and store variable 
-    temp = Math.round(json.main.temp);
+      // Get temp and store variable 
+      temp = Math.round(json.main.temp);
     
-    $("#temp").html(temp + "°<a href=\"#\" id=\"unit\">" + unit + "</a>");
+      $("#temp").html(temp + "°<a href=\"#\" id=\"unit\">" + unit + "</a>");
+      $("#humidity").html(json.main.humidity + "% Humidity");
+      $("#weather").html(json.weather[0].main);
     
-  $("#humidity").html(json.main.humidity + "% Humidity");
-    $("#weather").html(json.weather[0].main);
-    
-    $("#icon").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+      $("#icon").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
   });
 }
 
